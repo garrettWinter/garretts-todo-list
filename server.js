d1558ef90fee5b24636b09b3394bcae1bab22d88
Garrett Winter <Garrett_Winter@hotmail.com>
2023-01-15 18:32:04 -0600

    Got the notes route working

--- server.js
+++ server.js
@@ -12,4 +12,11 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+//ROUTES --- THESE WILL NEED TO MOVE
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
