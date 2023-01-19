0edec3e22072892e03cd1bc74474fda735de474d
Garrett Winter <Garrett_Winter@hotmail.com>
2023-01-19 11:08:09 -0600

    added path to server.js file to see if that will resolve heroku error.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const PORT = process.env.PORT || 3001;
-
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
